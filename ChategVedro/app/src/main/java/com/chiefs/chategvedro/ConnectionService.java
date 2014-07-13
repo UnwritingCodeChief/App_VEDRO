8be79d86d31807341a92c2f7adb9add4838d0c33
Сostal Сactus <8841siegheil1488@gmail.com>
2014-07-13 20:52:04 +0400

    Broadcasting хотя хз хуй знает не компилил даж

--- ChategVedro/app/src/main/java/com/chiefs/chategvedro/ConnectionService.java
+++ ChategVedro/app/src/main/java/com/chiefs/chategvedro/ConnectionService.java
@@ -6,17 +6,24 @@ import android.os.Binder;
 import android.os.IBinder;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class ConnectionService extends Service {
     private ArrayList<Interlocutor> interlocutors;
-    private boolean working;
+    private boolean serverLoop;
     private ServerSocket serverSocket;
 
+    private boolean clientLoop;
+    private Socket clientSocket;
+    private InputStream clientInputStream;
+    private OutputStream clientOutputStream;
+
     public ConnectionService() {
-        working = false;
+        serverLoop = false;
     }
 
     class LocalBinder extends Binder {
@@ -33,20 +40,38 @@ public class ConnectionService extends Service {
     }
 
     public void stopServer() throws IOException {
-        working = false;
-        for (Interlocutor interlocutor: interlocutors) {
-            interlocutor.getSocket().close();
+        if (serverLoop) {
+            serverLoop = false;
+            for (Interlocutor interlocutor : interlocutors) {
+                interlocutor.getSocket().close();
+            }
         }
     }
 
     public void startAsServer(int port) throws IOException {
-        if (!working) {
-            working = true;
+        if (!serverLoop) {
             serverSocket = new ServerSocket(port);
-            while (working) {
+            interlocutors = new ArrayList<Interlocutor>();
+            serverLoop = true;
+
+            while (serverLoop) {
                 Socket tempsocket = serverSocket.accept();
                 interlocutors.add(new Interlocutor(tempsocket,"John"));
             }
+        }
+    }
+
+    public void sendMessage (String message) throws IOException {
+        if (serverLoop) {
+            for (Interlocutor interlocutor : interlocutors) {
+                interlocutor.getOutputStream().write(message.getBytes());
+            }
+        }
+        else if (clientLoop) {
+            clientOutputStream.write(message.getBytes());
+        }
+        else {
+            //TODO: ke?
         }
     }
 }
--- ChategVedro/app/src/main/java/com/chiefs/chategvedro/Interlocutor.java
+++ ChategVedro/app/src/main/java/com/chiefs/chategvedro/Interlocutor.java
@@ -2,6 +2,9 @@ package com.chiefs.chategvedro;
 
 import android.content.Intent;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 /**
@@ -10,9 +13,13 @@ import java.net.Socket;
 public class Interlocutor {
     private Socket socket;
     private String name;
+    private InputStream inputStream;
+    private OutputStream outputStream;
 
-    public Interlocutor(Socket socket, String name) {
+    public Interlocutor(Socket socket, String name) throws IOException {
         this.socket = socket;
+        this.inputStream = socket.getInputStream();
+        this.outputStream = socket.getOutputStream();
         this.name = name;
     }
 
@@ -22,5 +29,13 @@ public class Interlocutor {
 
     public String getName() {
         return name;
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    public OutputStream getOutputStream() {
+        return outputStream;
     }
 }
